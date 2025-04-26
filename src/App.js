91fba40b00e64d43fa08bd7102a385094beebc6c
Arun <arunmeena.738950@gmail.com>
2025-04-26 22:56:08 +0530

    QR code functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import './Styles/Common.scss';
 import { Provider } from "react-redux";
 import { store, persistor } from "./Redux/store";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate, useNavigate } from "react-router-dom";
 import ItemComponent from './Component/Items/ItemComponent';
 import Setting from './Component/Setting';
 import Sidebar from './Component/Sidebar/Sidebar';
@@ -14,10 +14,20 @@ import Login from './Component/SignupLogin/Login';
 import PrivateRoute from './Component/PrivateRoute';
 import { PersistGate } from 'redux-persist/integration/react';
 
+const isMobileDevice = () => {
+  return window.innerWidth <= 768;
+};
+
 const AppContent = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const isAuthPage = location.pathname === "/sign-up" || location.pathname === "/sign-in";
 
+  useEffect(() => {
+    if (isMobileDevice() && (location.pathname === '/sign-up' || location.pathname === '/sign-in')) {
+      navigate('/customer', { replace: true });
+    }
+  }, [location]);
   return (
     <div className="main-app">
       {!isAuthPage && <Header />}
@@ -26,9 +36,10 @@ const AppContent = () => {
         <Routes>
           <Route path="/sign-up" element={<Signup />} />
           <Route path="/sign-in" element={<Login />} />
+          <Route path="/customer" element={<div>Customer Landing Page</div>} /> 
 
           {!isAuthPage && (
-            <Route element={<Sidebar />}>
+            <Route element={!isMobileDevice()?<Sidebar />:<div></div>}>
               <Route
                 path="/"
                 element={
