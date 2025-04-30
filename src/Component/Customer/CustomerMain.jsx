03aa2de975ea540cf82f1ecc82e93b0041d2c0bb
Arun <arunmeena.738950@gmail.com>
2025-05-01 00:26:00 +0530

    mobile screen input field

--- src/Component/Customer/CustomerMain.jsx
+++ src/Component/Customer/CustomerMain.jsx
@@ -2,31 +2,46 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
 import './customer.scss';
 import BottomPopup from './BottomPopup';
 import { useLocation } from 'react-router-dom';
 import { getCustomerDataAction } from '../../Redux/Action';
+import { TextField,InputAdornment } from '@mui/material';
 
 
 const CustomerMain = () => {
-      const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const customerMenuData = useSelector((state) => state?.customerMenu?.data);
   const dataItem = customerMenuData?.data || [];
   const [open, setOpen] = useState(false);
   const location = useLocation();
   const businessID = location.state?.businessID;
-useEffect(()=>{
-  dispatch(getCustomerDataAction(businessID));
-},[]);
+  useEffect(() => {
+    dispatch(getCustomerDataAction(businessID));
+  }, []);
   return (
     <div className="customer-container text-white">
-      <div className='d-flex'>
-        <div className='ml-1'>
-        <FilterAltIcon onClick={() => setOpen(true)}/>
-        </div>
-        <div style={{right:"20px",position:"absolute"}}>
-        <ShoppingCartIcon onClick={() => setOpen(true)}/>
-        </div>
+    <div className='customer-header d-flex align-items-center justify-content-between mb-3 px-2'>
+      <FilterAltIcon onClick={() => setOpen(true)} style={{ color: 'white',fontSize:"35px" }} className='m-1'/>
+      <ShoppingCartIcon onClick={() => setOpen(true)} style={{ color: 'white',fontSize:"35px" }} className='m-1'/>
+      <TextField
+        placeholder="Search"
+        variant="outlined"
+        className="search-input"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+          style: {
+            backgroundColor: "white",
+            borderRadius: "8px"
+          }
+        }}
+      />
+      
     </div>
   
     <div className="customer-card-wrapper">
@@ -45,8 +60,10 @@ useEffect(()=>{
         </div>
       ))}
     </div>
-    <BottomPopup open={open} onClose={() => setOpen(false)} onOpen={() => {}} />
+  
+    <BottomPopup open={open} onClose={() => setOpen(false)} onOpen={() => { }} />
   </div>
+  
   )
 }
 
