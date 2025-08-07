fe2d80015a82e05671e4d312418d1e310cd5462c
Arun <arunmeena.738950@gmail.com>
2025-08-08 01:00:14 +0530

    added lazzy loading in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import './App.css';
-import './Styles/Common.scss';
+import React, { lazy, Suspense } from "react";
+import "./App.css";
+import "./Styles/Common.scss";
 import { Provider } from "react-redux";
 import { store, persistor } from "./Redux/store";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import ItemTable from './Component/Items/ItemTable';
-import Setting from './Component/Setting';
-import Sidebar from './Component/Sidebar/Sidebar';
-import Header from './Component/Navbar/Header';
-import Signup from './Component/SignupLogin/Signup';
-import Login from './Component/SignupLogin/Login';
-import PrivateRoute from './Component/PrivateRoute';
-import { PersistGate } from 'redux-persist/integration/react';
-import CustomerMain from './Component/Customer/CustomerMain';
-import CustomerRedirectPage from './Component/Customer/CustomerRedirectPage';
-import OrderList from './Routes/OrderList/OrderList';
-import ItemCreateEdit from './Component/Items/ItemCreateEdit';
+import PrivateRoute from "./Component/PrivateRoute";
+import { PersistGate } from "redux-persist/integration/react";
+const ItemTable = lazy(() => import("./Component/Items/ItemTable"));
+const Setting = lazy(() => import("./Component/Setting"));
+const Sidebar = lazy(() => import("./Component/Sidebar/Sidebar"));
+const Header = lazy(() => import("./Component/Navbar/Header"));
+const Signup = lazy(() => import("./Component/SignupLogin/Signup"));
+const Login = lazy(() => import("./Component/SignupLogin/Login"));
+const CustomerMain = lazy(() => import("./Component/Customer/CustomerMain"));
+const CustomerRedirectPage = lazy(() =>
+  import("./Component/Customer/CustomerRedirectPage")
+);
+const OrderList = lazy(() => import("./Routes/OrderList/OrderList"));
+const ItemCreateEdit = lazy(() => import("./Component/Items/ItemCreateEdit"));
 
 const isMobileDevice = () => {
   return window.innerWidth <= 768;
@@ -24,68 +26,88 @@ const isMobileDevice = () => {
 
 const AppContent = () => {
   const location = useLocation();
-  const isAuthPage = location.pathname === "/sign-up" || location.pathname === "/sign-in" || isMobileDevice();
+  const isAuthPage =
+    location.pathname === "/sign-up" ||
+    location.pathname === "/sign-in" ||
+    isMobileDevice();
 
   return (
     <div className="main-app">
-      {!isAuthPage && <Header />}
+      <Suspense fallback={<div>Loading...</div>}>
+        {!isAuthPage && <Header />}
 
-      <div className={!isAuthPage ? `main-content-scrollable` : ""}>
-        <Routes>
-          {!isMobileDevice() &&
-            <>
-              <Route path="/sign-up" element={<Signup />} />
-              <Route path="/sign-in" element={<Login />} />
-            </>
-          }
-          <Route path="/customer/:id" element={<CustomerRedirectPage />} />
-          <Route path="/customer" element={<CustomerMain />} />
+        <div className={!isAuthPage ? `main-content-scrollable` : ""}>
+          <Routes>
+            {!isMobileDevice() && (
+              <>
+                <Route path="/sign-up" element={<Signup />} />
+                <Route path="/sign-in" element={<Login />} />
+              </>
+            )}
+            <Route path="/customer/:id" element={<CustomerRedirectPage />} />
+            <Route path="/customer" element={<CustomerMain />} />
 
-          {!isAuthPage && (
-            <Route element={(!isMobileDevice() || location.pathname === '/customer') ? <Sidebar /> : <div></div>}>
+            {!isAuthPage && (
               <Route
-                path="/merchant"
                 element={
-                  <PrivateRoute>
-                    <ItemTable />
-                  </PrivateRoute>
+                  !isMobileDevice() || location.pathname === "/customer" ? (
+                    <Sidebar />
+                  ) : (
+                    <div></div>
+                  )
                 }
-              />
-               <Route
-                path="/merchant/orderlist"
-                element={
-                  <PrivateRoute>
-                    <OrderList />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/merchant/setting"
-                element={
-                  <PrivateRoute>
-                    <Setting />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/merchant/item"
-                element={
-                  <PrivateRoute>
-                    <ItemCreateEdit />
-                  </PrivateRoute>
-                }
-              />
-            </Route>
-          )}
+              >
+                <Route
+                  path="/merchant"
+                  element={
+                    <PrivateRoute>
+                      <ItemTable />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/merchant/orderlist"
+                  element={
+                    <PrivateRoute>
+                      <OrderList />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/merchant/setting"
+                  element={
+                    <PrivateRoute>
+                      <Setting />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/merchant/item"
+                  element={
+                    <PrivateRoute>
+                      <ItemCreateEdit />
+                    </PrivateRoute>
+                  }
+                />
+              </Route>
+            )}
 
-          {/* Redirect unknown paths */}
-          <Route path="*" element={<Navigate to={!isMobileDevice() ? "/merchant" : "/customer"} replace />} />
-        </Routes>
-      </div>
+            {/* Redirect unknown paths */}
+            <Route
+              path="*"
+              element={
+                <Navigate
+                  to={!isMobileDevice() ? "/merchant" : "/customer"}
+                  replace
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Suspense>
     </div>
   );
 };
-
 
 const App = () => {
   return (
